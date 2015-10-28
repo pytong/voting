da936696745605dea3d0af884ad38a9f920c4040
pytong <pytong07@gmail.com>
2015-10-28 02:05:33 +0000

    Added register form

--- app/controllers/clickController.client.js
+++ app/controllers/clickController.client.js
@@ -4,10 +4,7 @@
 
    var addButton = document.querySelector('.btn-add');
    var deleteButton = document.querySelector('.btn-delete');
-   var clickNbr = document.querySelector('#click-nbr');
    var apiUrl = appUrl + '/api/:id/clicks';
-
-   ajaxFunctions.ready(ajaxFunctions.ajaxRequest('GET', apiUrl, updateClickCount));
 
    // addButton.addEventListener('click', function () {
 
