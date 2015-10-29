07d66dca66fa8119493c14ce1c62693c6ceb5f67
pytong <pytong07@gmail.com>
2015-10-29 21:24:36 +0000

    Validate password

--- app/config/passport.js
+++ app/config/passport.js
@@ -76,12 +76,13 @@ module.exports = function (passport) {
 			usernameField: 'email'
 		},
 	    function(username, password, done) {
-	        User.findOne({ username: username }, function(err, user) {
+	        User.findOne({ email: username }, function(err, user) {
 		        if (err) { return done(err); }
+
 		        if (!user) {
 		            return done(null, false, { message: 'Incorrect username.' });
 		        }
-		        if (!user.validPassword(password)) {
+		        if (user.password !== password) {
 		            return done(null, false, { message: 'Incorrect password.' });
 		        }
 		        return done(null, user);
