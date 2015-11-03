78be6fb0c65fdcb7dc95aa17f5f4e707695c658d
pytong <pytong07@gmail.com>
2015-11-03 21:14:09 +0000

    Make sure there are at least 2 choices in the poll

--- app/controllers/pollController.js
+++ app/controllers/pollController.js
@@ -1,14 +1,18 @@
 (function(app) {
     app.controller("PollController", ["$scope", function($scope) {
 
-        $scope.choices = [];
+        $scope.choices = [{"id": "choice1"}, {"id": "choice2"}];
 
         $scope.addChoice = function() {
+            if($(".choice").last().val() === "") { return; }
+
             var newItemNo = $scope.choices.length + 1;
             $scope.choices.push({"id": "choice" + newItemNo});
         }
 
         $scope.removeChoice = function() {
+            if($scope.choices.length < 3) { return; }
+
             var lastItem = $scope.choices.length - 1;
             $scope.choices.splice(lastItem);
         }
