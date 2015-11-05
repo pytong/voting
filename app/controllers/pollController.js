498301730ac0176165ca6068ac8962996257ffe1
pytong <pytong07@gmail.com>
2015-11-05 06:34:43 +0000

    Fixed poll form

--- app/controllers/pollController.js
+++ app/controllers/pollController.js
@@ -1,20 +1,21 @@
 (function(app) {
     app.controller("PollController", ["$scope", "PollService", function($scope, PollService) {
 
-        $scope.choices = [{"id": "choice1"}, {"id": "choice2"}];
+        $scope.poll = {};
+        $scope.poll.choices = [{"id": "choice1"}, {"id": "choice2"}];
 
         $scope.addChoice = function() {
-            if($(".choice").last().val() === "") { return; }
+            if($(".choice-field").last().val() === "") { return; }
 
-            var newItemNo = $scope.choices.length + 1;
-            $scope.choices.push({"id": "choice" + newItemNo});
+            var newItemNo = $scope.poll.choices.length + 1;
+            $scope.poll.choices.push({"id": "choice" + newItemNo});
         }
 
         $scope.removeChoice = function() {
-            if($scope.choices.length < 3) { return; }
+            if($scope.poll.choices.length < 3) { return; }
 
-            var lastItem = $scope.choices.length - 1;
-            $scope.choices.splice(lastItem);
+            var lastItem = $scope.poll.choices.length - 1;
+            $scope.poll.choices.splice(lastItem);
         }
 
         $scope.createPoll = function(question, choices) {
